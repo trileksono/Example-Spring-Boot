85dfeb0f9aa5913dc6318ce32c3824c37e3e9d92
tri <treesekatamavia@yahoo.com>
2016-03-27 21:34:07 +0700

    Buku belom kelar

--- src/main/java/com/tri/leksono/dao/BukuDAO.java
+++ src/main/java/com/tri/leksono/dao/BukuDAO.java
@@ -6,6 +6,8 @@
 package com.tri.leksono.dao;
 
 import com.tri.leksono.entity.Buku;
+import java.awt.print.Book;
+import java.util.List;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 /**
@@ -13,5 +15,6 @@ import org.springframework.data.repository.PagingAndSortingRepository;
  * @author tri
  */
 public interface BukuDAO extends PagingAndSortingRepository<Buku, String> {
-    
+    List<Book> findByNamaBuku(String namaBuku);
+    List<Book> findByPengarang(String pengarang);
 }
---
+++ src/test/java/com/tri/leksono/sql/book.sql
@@ -0,0 +1,11 @@
+insert into tbl_buku values
+('001','Java fundamental 1','Tidak diketahui','2008'),
+('002','Java fundamental 2','Tidak diketahui','2009'),
+('003','Pro Java ME Apps','Ovidiu Iliescu','2011'),
+('004','Java EE 6 Pocket Guide','Arun Gupta','2012'),
+('005','Java Cookbook, 2nd Edition','Ian F. Darwin','2004'),
+('006','Sams Teach Yourself Java in 24 Hours, 6th Edition','Rogers Cadenhead','2011')
+('007','Node.js Design Patterns','Mario Casciaro','2014'),
+('008','Node Cookbook','David Mark Clements','2012'),
+('009','Node.js By Example','Krasimir Tsonev','2015'),
+('010','Node Cookbook, 2nd Edition','David Mark Clements','2014');
